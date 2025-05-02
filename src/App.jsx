fa30fc3652a6f45c7174e3be371dc2de1ae214e5
André Pereira <andreporteira@gmail.com>
2025-05-02 14:36:48 -0300

    Validações prontas

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [error, setError] = useState('')
+  const [success, setSuccess] = useState(false)
 
+  const validateEmail = (email) => {
+    return /\S+@\S+\.\S+/.test(email)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setError('')
+    setSuccess('')
+
+    if (!email || !password || !confirmPassword) {
+      return setError('Preencha todos os campos')
+    }
+    if (password !== confirmPassword) {
+      return setError('Senhas não conferem')
+    }
+    if (!validateEmail(email)) {
+      return setError('Email inválido')
+    }
+    if (password.length < 6) {
+      return setError('Senha deve ter pelo menos 6 caracteres')
+    }
+    const newUser = {
+      email,
+      password
+    }
+    localStorage.setItem('user', JSON.stringify(newUser))
+    setSuccess('Cadastro realizado com sucesso!')
+
+  }
+
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    { error && <p style={{ color: 'red' }}>{error}</p> }
+    { success && <p style={{ color: 'green' }}>Cadastro realizado com sucesso!</p> }
+    <form>
+      <label htmlFor="email">Email: </label>
+      <input type='text' id='email' value={email} onChange={(e) => setEmail(e.target.value)}  /><br></br>
+      <label htmlFor='password'>Senha: </label>
+      <input type='password' id='password' value={password} onChange={(e) => setPassword(e.target.value)} /><br></br>
+      <label htmlFor='confirmPassword'>Confirmar senha: </label>
+      <input type='password' id='confirmPassword' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} /><br></br>
+      <button type='submit' onClick={handleSubmit}>Enviar</button>    
+    </form>
     </>
   )
+
 }
 
 export default App
